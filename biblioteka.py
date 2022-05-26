4ea720adca1e8745ef1876aafdd9a47c57e0fe69
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-05-26 13:02:13 +0200

    added commentaries

--- biblioteka.py
+++ biblioteka.py
@@ -1,6 +1,7 @@
 import random
 from datetime import date
 
+#Klasa bazowa
 class Base:
     def __init__(self, type_card, title, year):
         self.type_card = type_card
@@ -10,7 +11,7 @@ class Base:
         
 
 
-
+#Klasa movies
 class Movieinformation(Base):
     def __init__(self, title, year, type, numplays):
         super().__init__("Movie",title, year)
@@ -26,7 +27,9 @@ class Movieinformation(Base):
     def info(self):
         informations = self.title + "\t" + f"({str(self.year)})"
         return informations
-        
+
+
+#Klasa series
 class Seriesinformation(Base):
     def __init__(self, title, year, type, episodenum, seasonnum, numplays):
         super().__init__("Series",title, year)
@@ -35,20 +38,23 @@ class Seriesinformation(Base):
         self.seasonnum = seasonnum
         self.numplays = numplays
     
+    #Wyświetlenia
     def play(self):
         self.numplays += 1
         return self.numplays
-
+    
+    #Informacje
     def info(self):
         informations = self.title + " " + "S" + str(self.seasonnum) + "E" + str(self.episodenum)
         return informations
 
 
 
-
+#Klasa biblioteki
 class Library:
     movieserialsbase = []
-
+    
+    #Wyswietlanie series
     def get_series(self):
         print("Series list: ")
         for i in self.movieserialsbase:
@@ -56,25 +62,32 @@ class Library:
                 print(i)
             sorted(i)
     
+    #Wyświetlanie movies
     def get_movies(self):
         print("Movies list: ")
-        for i in self.movieserialbase:
+        for i in self.movieserialsbase:
             if i.type == 'Movies':
                 print(i)
             sorted(i)
 
+    
+    #Wyszukiwanie z listy
     def search(self):
         find = input("Find Movie or serial by its title: ")
         for i in self.movieserialsbase:
             if find == i.title:
                 print(i.title)
 
+    
+    #Generowanie wyświetleń
     def generate_views(self):
-        moviesample = Movieinformation.choice(self.title)
-        moviesample.title
+        add_movies(self)
+        movie.movies.title
+        moviesample = random.sample(self.title,1)
+        print(moviesample)
         seriessample = Seriesinformation.choice(self.title)
-        seriessample.title
-        sample = random.sample(zip(moviesample.title,seriessample.title), 1)
+        seriessample.title = seriessample
+        sample = random.sample(zip(moviesample,seriessample), 1)
         print(sample)
         
         #sample = random.sample(self.movieserialsbase(self.title), 1)
@@ -89,12 +102,14 @@ class Library:
     
 
 
-
+    #Wyświetlenia *10
     def views_times_ten(self, generate_views):
         for i in range(11):
             generate_views()
             i += 1
 
+    
+    #Najpopularniejsze tytuły
     def top_titles(self):
         viewcount = 0
         content_type = input("Show top titles of movies or series? ")
@@ -117,6 +132,7 @@ class Library:
                                 print(i.title)
                                 x += 1
     
+    #Dodawanie movies
     def add_movies(self):
         movies_title = input("Ttile of movie: ")
         movies_year = int(input("Movie year: "))
@@ -127,7 +143,7 @@ class Library:
 
         
         
-            
+    #Dodawanie series        
     def add_series(self):
         series_title = input("Title of series: ")
         series_year = int(input("Series year: "))
@@ -138,11 +154,13 @@ class Library:
         newSeries = Seriesinformation(series_title, series_year, series_type,series_season, series_episode, series_numplays)
         self.movieserialsbase.append(newSeries)    
         
+    
+    #Pokazanie listy movieserialsbase
     def show_library(self):
         print("Library of movies and series: ")
         for i in self.movieserialsbase:
             if i.type_card == 'Movie':
-                print(i)
+                print(i.info())
             elif i.type_card == 'Series':
                 print(i.info())
         
